9ec116ba2c4b01ca0fd232123df913531c27722c
Jack Kellenberger <jackson@kellenberger.io>
2022-07-05 21:37:29 -0400

    Remove original

--- stock_picker/stock_picker.rb
+++ stock_picker/stock_picker.rb
@@ -1,26 +1,3 @@
-# def stock_picker(prices)
-#   min_max = prices.minmax
-#   unless prices.find_index(min_max[0]) < prices.find_index(min_max[1])
-#     buy_days = prices[0, prices.length-1]
-#     profit = 0
-#     buy_days.each_index do |buy|
-#       sell_days = prices[buy+1, prices.length]
-#       sell_days.each_index do |sell|
-#         margin = sell_days[sell] - buy_days[buy]
-#         if margin > profit 
-#           profit = margin
-#           min_max[0] = buy_days[buy]
-#           min_max[1] = sell_days[sell]
-#         end
-#       end
-#     end
-#   end
-#   buy_sell_days = []
-#   buy_sell_days[0] = prices.find_index(min_max[0])
-#   buy_sell_days[1] = prices.find_index(min_max[1])
-#   return buy_sell_days
-# end
-
 def stock_picker(prices)
   pairs = prices.combination(2).to_a
   profit = 0
@@ -34,6 +11,3 @@ def stock_picker(prices)
   end
   return best_days
 end
-
-
-p stock_picker([17,3,6,9,15,8,6,1,10])
