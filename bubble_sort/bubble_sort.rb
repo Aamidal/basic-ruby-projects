4a697400be77c98aba0fcb2a22886b6be438b87c
Jack Kellenberger <jackson@kellenberger.io>
2022-07-06 11:07:30 -0400

    Replace boolean with new_n

--- bubble_sort/bubble_sort.rb
+++ bubble_sort/bubble_sort.rb
@@ -1,20 +1,19 @@
 def bubble_sort(array)
   a = array
   n = a.length
-  swapped = ''
-  until swapped == false
-    swapped = nil
+  until n <= 0 do
+    new_n = 0
     for i in 1..n-1 do
       if a[i-1] > a[i] then
         array[i-1], array[i] = array[i], array[i-1]
-        swapped = true
+        new_n = i
       end
     end
-    swapped = false if swapped == nil
+    n = new_n
   end
   return a
 end
 
-p bubble_sort([4, 3, 78, 2, 0, 2])
+p bubble_sort([4,3,78,2,0,2])
 # > bubble_sort([4,3,78,2,0,2])
 # => [0,2,2,3,4,78]
