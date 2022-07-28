f7ad5915f58fec5ac2cff24f0057f1927ed3fb11
Jack Kellenberger <jackson@kellenberger.io>
2022-07-28 16:04:18 -0400

    Remove counter variable from collatz

--- collatz.rb
+++ collatz.rb
@@ -2,7 +2,11 @@
 ## if n is even, repeat on n/2
 ## if n is odd, repeat on 3n+1
 
-def collatz(n, steps = 1)
-  return "Steps: #{steps}" if n == 1
-  n.even? ? collatz(n/2, steps+1) : collatz(3*n+1, steps+1)
+def collatz(n)
+  return 0 if n == 1
+  n.even? ? 1 + collatz(n/2) : 1 + collatz(3*n+1)
+end
+
+[1, 2, 3, 4, 5, 6, 7, 8, 15, 27, 50].each do |test|
+  puts "Testing #{test}: #{collatz(test)} steps."
 end
