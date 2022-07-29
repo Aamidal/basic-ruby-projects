8b7e4472ee77fe7292d7a793c3e7ebb16abde25d
Jack Kellenberger <jackson@kellenberger.io>
2022-07-29 13:34:22 -0400

    Clean up formatting
    Adjust merge conditional from length > 0 to .positive?

--- recursion/merge_sort.rb
+++ recursion/merge_sort.rb
@@ -1,36 +1,31 @@
 def merge_sort(array)
-    return array if array.length < 2
+  return array if array.length < 2
 
-    left = []
-    right = []
+  left = []
+  right = []
 
-    array.each_with_index { |num, idx| idx < array.length/2 ? left.push(num) : right.push(num) }
+  array.each_with_index { |num, idx| idx < array.length / 2 ? left.push(num) : right.push(num) }
 
-    left = merge_sort(left)
-    right = merge_sort(right)
+  left = merge_sort(left)
+  right = merge_sort(right)
 
-    return merge(left, right)
+  merge(left, right)
 end
 
 def merge(left, right)
-    result = []
+  result = []
 
-    while left.length > 0 && right.length > 0 do
-        left.first <= right.first ? result.push(left.shift) : result.push(right.shift)
-    end
+  while left.length.positive? && right.positive?
+    left.first <= right.first ? result.push(left.shift) : result.push(right.shift)
+  end
 
-    while left.length > 0 do
-        result.push(left.shift)
-    end
-    while right.length > 0 do
-        result.push(right.shift)
-    end
-    
-    return result
+  result.push(left.shift) while left.positive?
+  result.push(right.shift) while right.length.positive?
+
+  result
 end
 
-num_list = [5, 4, 6, 7, 1, 2, 10, 13, 0, 2, 4, 7, 9, 2, 3, 45, 683, 39, 9999999, 493, 238]
+num_list = [5, 4, 6, 7, 1, 2, 10, 13, 0, 2, 4, 7, 9, 2, 3, 45, 683, 493, 238]
 
 puts "Original list: #{num_list}."
 puts "Sorted list: #{merge_sort(num_list)}"
-
